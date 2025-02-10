7817827681b1843fc9106607d5cfeecb83429c38
Mateus Fernandes <fernandesmateus2002@gmail.com>
2025-02-10 20:06:44 -0300

    adicionando mais funcionalidades e corrigindo erros

--- scripts.js/main.js
+++ scripts.js/main.js
@@ -18,4 +18,46 @@ function carregarPergunta () {
 
 }
 
+window.verificarResposta = function verificarResposta (opcaoSelecionada){
+    const pergunta = perguntas[perguntaAtual];
+
+    if(opcaoSelecionada === pergunta.respostaCorreta){
+        pontuacao++;
+    }
+
+    perguntaAtual++;
+
+    document.getElementById("pontuacao").innerText = `Pontuação: ${pontuacao}`;
+
+    if(perguntaAtual < perguntas.length){
+        carregarPergunta();
+    } else {
+        resultado();
+    }
+}
+
+function resultado (){
+    const resultadoMensagem = document.getElementById("resultadoMensagem");
+    resultadoMensagem.innerText = `Você acertou ${pontuacao} de ${perguntas.length} perguntas realizadas!`;
+
+    const modal = document.getElementById("modalResultado");
+    modal.style.display = "block";
+
+    const closeModal = document.getElementById("closeModal");
+    closeModal.onclick = () => modal.style.display = "none";
+
+}
+
+window.reiniciarQuiz = function reiniciarQuiz() {
+
+    if(perguntaAtual === 0){
+        return;
+    }
+
+    perguntaAtual = 0;
+    pontuacao = 0;
+    carregarPergunta();
+}
+
+
 carregarPergunta();
