c96fa512aec017e60d89ec74f08a7312db1b59fb
Mateus Fernandes <fernandesmateus2002@gmail.com>
2025-02-10 19:01:37 -0300

    adicionando funcionalidade carregarPergunta

--- scripts.js/main.js
+++ scripts.js/main.js
@@ -5,15 +5,17 @@ let pontuacao = 0;
 
 function carregarPergunta () {
     const elementoPergunta = document.getElementById("pergunta");
-    const elementoOpcoes = document.querySelectorAll(".options");
+    const elementoOpcoes = document.querySelectorAll(".option");
     const pergunta = perguntas[perguntaAtual];
 
+    elementoPergunta.innerText = pergunta.pergunta;
+    
     elementoOpcoes.forEach((opcao, index) => {
         opcao.innerText = pergunta.respostas[index];
     })
 
     document.getElementById("pontuacao").innerText = `Pontuação: ${pontuacao}`;
-    
+
 }
 
-
+carregarPergunta();
--- scripts.js/perguntas.js
+++ scripts.js/perguntas.js
@@ -40,3 +40,5 @@
         respostaCorreta: 2
     },
 ];
+
+    
